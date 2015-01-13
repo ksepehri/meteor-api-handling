21c9c9713d46a13e1f169d7e97e7c4bff878ed58
ninja <ninja@blowdart.local>
2015-01-12 20:52:12 -0600

    added tv,movie feeds

--- my_cool_app.js
+++ my_cool_app.js
@@ -37,24 +37,87 @@ if (Meteor.isClient) {
 
 Template.tv.helpers({
     stuff: function () {
-      return Session.get("musicStuff");
+      return Session.get("tvStuff");
     }
   });
 
   Template.tv.events({
     'click button': function () {
-     console.log(artist.value);
+     console.log(show.value);
       Meteor.call("searchTVShow", show.value, function(error, results) {
         console.log(results); //results.data should be a JSON object
         
         //artist class
         function Show(name,image) {
           this.name = name;
-          this.image = image;
+          this.image = "http://thetvdb.com/banners/" + image;
         }
         
+        shows = [];
+
+        results.Data.Series.forEach(function (item) {
+          shows.push(new Show(item.SeriesName[0],item.banner[0]))
+        });
         
-        Session.set("tvStuff", results);
+        Session.set("tvStuff", shows);
+    });
+    }
+  });
+
+Template.movie.helpers({
+    stuff: function () {
+      return Session.get("movieStuff");
+    }
+  });
+
+  Template.movie.events({
+    'click button': function () {
+     console.log(movie.value);
+      Meteor.call("searchMovie", movie.value, function(error, results) {
+        console.log(results); //results.data should be a JSON object
+        
+        //artist class
+        function Movie(name,image) {
+          this.name = name;
+          this.image = "http://image.tmdb.org/t/p/w92" + image;
+        }
+        
+        movies = [];
+
+        results.results.forEach(function (item) {
+          movies.push(new Movie(item.original_title,item.poster_path))
+        });
+        
+        Session.set("movieStuff", movies);
+    });
+    }
+  });
+
+  Template.tv2.helpers({
+    stuff: function () {
+      return Session.get("tv2Stuff");
+    }
+  });
+
+  Template.tv2.events({
+    'click button': function () {
+     console.log(tv2.value);
+      Meteor.call("searchTV2", tv2.value, function(error, results) {
+        console.log(results); //results.data should be a JSON object
+        
+        //artist class
+        function Movie(name,image) {
+          this.name = name;
+          this.image = "http://image.tmdb.org/t/p/w92" + image;
+        }
+        
+        movies = [];
+
+        results.results.forEach(function (item) {
+          movies.push(new Movie(item.original_title,item.poster_path))
+        });
+        
+        Session.set("movieStuff", movies);
     });
     }
   });
@@ -71,11 +134,29 @@ if (Meteor.isServer) {
         searchTVShow: function (show) {
             this.unblock();
             result = Meteor.http.call("GET", "http://thetvdb.com/api/GetSeries.php?seriesname=" + show.replace(" ","+"));
-            //console.dir(result);
+            //console.dir(result.content);
+
+            json_result = "";
             
-            xml2js.parseString(result, function (err, results) {
-                console.dir(results);
+            xml2js.parseString(result.content, function (err, results) {
+                json_result = JSON.stringify(results);
             });
+
+            return JSON.parse(json_result);
+        },
+        searchMovie: function (movie) {
+            this.unblock();
+            result = Meteor.http.call("GET", "http://api.themoviedb.org/3/search/movie?year=2014&api_key=" + process.env.MOVIEDB_KEY + "&query=" + movie.replace(" ","+"));
+            //console.dir(result.content);
+
+            return JSON.parse(result.content);
+        },
+        searchTV2: function (show) {
+            this.unblock();
+            result = Meteor.http.call("GET", "http://api.themoviedb.org/3/search/tv?api_key=" + process.env.MOVIEDB_KEY + "&query=" + show.replace(" ","+"));
+            //console.dir(result.content);
+
+            return JSON.parse(result.content);
         }
     });
 }
