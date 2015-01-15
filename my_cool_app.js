4bab4eb12d86cad1e1a374e0a9748a773ff2ee8d
ksepehri <ksepehri@users.noreply.github.com>
2015-01-15 23:13:56 +0000

    updated music search and tv season lookup

--- my_cool_app.js
+++ my_cool_app.js
@@ -18,66 +18,35 @@ if (Meteor.isClient) {
     'click button': function () {
      console.log(artist.value);
       Meteor.call("searchArtist", artist.value, function(error, results) {
-        console.log(results.results.artistmatches.artist); //results.data should be a JSON object
+        //console.log(results.results.artistmatches.artist);        
+   //results.results.artistmatches.artist.forEach(function (item) {
+        console.log(results.artists); //results.data should be a JSON object
         
         //artist class
-        function Artist(name,image) {
+        function Artist(name,image,id) {
           this.name = name;
           this.image = image;
+          this.id = id;
         }
         
         artists = [];
         
-        results.results.artistmatches.artist.forEach(function (item) {
-          artists.push(new Artist(item.name,item.image[3]["#text"]))
+        results.artists.forEach(function (item) {
+          artists.push(new Artist(item.name,null,item.id))
         });
         
         
         Session.set("musicStuff", artists);
     });
-    }
-  });
-
-
-Template.tv.helpers({
-    stuff: function () {
-      return Session.get("tvStuff");
-    }
-  });
-  
-  var getShows = function(value){
-    Meteor.call("searchTVShow", value, function(error, results) {
-        console.log(results); //results.data should be a JSON object
-        
-        //artist class
-        function Show(name,image) {
-          this.name = name;
-          this.image = (image === "") ? "":"http://thetvdb.com/banners/" + image;
-        }
-        
-        shows = [];
-        
-        if (typeof results.Data.Series !== 'undefined'){
-          results.Data.Series.forEach(function (item) {
-            shows.push(new Show(item.SeriesName[0],(typeof item.banner !== 'undefined') ? item.banner[0]: ""))
-          });
-          
-          Session.set("tvStuff", shows);
-        }
-    });
-  }
-
-  Template.tv.events({
-    'click button': function () {
-     console.log(show.value);
-     getShows(show.value);
     },
-    'keyup .show': function () {
-      if(show.value.length > 4)
-      console.log(show.value);
-      getShows(show.value);
+    'click div': function(){
+      console.log(this.name);
+       Meteor.call("searchAlbums", this.name, function(error, results) {
+         console.log(results);
+       });
     }
   });
+
 
 Template.movie.helpers({
     stuff: function () {
@@ -121,64 +90,81 @@ Template.movie.helpers({
         console.log(results); //results.data should be a JSON object
         
         //artist class
-        function Show(name,image,id,seasons) {
+        function Show(name,image,id) {
           this.id = id;
           this.name = name;
           this.image = (image === "null") ? "":"http://image.tmdb.org/t/p/w92" + image;
-          this.seasons = seasons;
         }
         
         shows = [];
-
-        results.results.forEach(function (item) {
-          if(item.poster_path !== null)
-            shows.push(new Show(item.name,item.poster_path,item.id,item.seasons))
-        });
+        var count = 0;
+        len = (results.results.length < 3) ? results.results.length: 3;
+        for (var i = 0; i < len; i++) {
+          item = results.results[i];
+          if(item.poster_path !== null) {
+            
+            shows.push(new Show(item.name,item.poster_path,item.id));
+          }
+        }
+        // results.results.forEach(function (item) {
+        //   if(count > 2)
+        //     return false;
+        //   if(item.poster_path !== null) {
+        //     shows.push(new Show(item.name,item.poster_path,item.id,item.seasons))
+        //     count++;
+        //   }
+        // });
         
         Session.set("tv2Stuff", shows);
     });
     },
     'click div': function () {
       console.log(this.id);
+      
+      Meteor.call("searchSeason2", this.id, function(error, results) {
+        console.log(results.seasons);
+        Session.set('seasons',results.seasons);
+        
+      });
     }
   });
   
+  Template.seasons.created = function() {
+    Session.setDefault("seasons", "");
+  }
   
-  var nba = function(){
-		return Nba.find().fetch().map(function(it){ return it.name; });
-	};
-
-	// simple example
-	Template.basic.helpers({
-		nba: function(){
-      Meteor.call("searchTV2", "sherlock", function(error, results) {
-        console.log(results.results); //results.data should be a JSON object
-        
-        //artist class
-        function Movie(name,image) {
-          this.name = name;
-          this.image = "http://image.tmdb.org/t/p/w92" + image;
-        }
-        
-        //movies = [];
-        if (Movies.find().count() === 0) {
-          results.results.forEach(function (item) {
-            Movies.insert(item);
-          });
-        }
-        return Movies.find().fetch();
-    });
-		}
-	});
+  Template.seasons.helpers({
+    seasons: function() {
+      return Session.get('seasons');
+    }
+  });
 }
 
 if (Meteor.isServer) {
     Meteor.methods({
         searchArtist: function (artist) {
             this.unblock();
-            result = Meteor.http.call("GET", "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist.replace(" ","+") + "&api_key=" + process.env.LASTFM_KEY + "&limit=3&format=json"); //"https://itunes.apple.com/search?term=" + artist.replace(" ","+") + "&entity=musicArtist&limit=3");
-            
+            //result = Meteor.http.call("GET", "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist.replace(" ","+") + "&api_key=" + process.env.LASTFM_KEY + "&limit=3&format=json"); //"https://itunes.apple.com/search?term=" + artist.replace(" ","+") + "&entity=musicArtist&limit=3");
+            result = Meteor.http.call("GET", "http://musicbrainz.org/ws/2/artist?query=" + artist.replace(" ","+") + "&fmt=json&limit=3");
             return JSON.parse(result.content);
+        },
+        searchAlbums: function (artist) {
+            this.unblock();
+            result = Meteor.http.call("GET", "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist.replace(" ","+") + "&api_key=" + process.env.LASTFM_KEY + "&format=json"); //"https://itunes.apple.com/search?term=" + artist.replace(" ","+") + "&entity=musicArtist&limit=3");
+            
+            json_albums = JSON.parse(result.content).topalbums.album;
+            
+            album_details = [];
+            
+            json_albums.forEach(function(item){
+              result = Meteor.http.call("GET", "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=" + artist.replace(" ","+") + "&album=" + item.name.replace(" ","+") + "&api_key=" + process.env.LASTFM_KEY + "&format=json");
+              album = JSON.parse(result.content).album;
+              releasedate = new Date(album.releasedate);
+              if(releasedate.getFullYear() === 2014)
+                album_details.push(album);
+            });
+            
+            return album_details;
         },
         searchTVShow: function (show) {
             this.unblock();
@@ -203,6 +189,14 @@ if (Meteor.isServer) {
         searchTV2: function (show) {
             this.unblock();
             result = Meteor.http.call("GET", "http://api.themoviedb.org/3/search/tv?api_key=" + process.env.MOVIEDB_KEY + "&query=" + show.replace(" ","+"));
+            //console.dir(result.content);
+
+            return JSON.parse(result.content);
+        },
+        searchSeason2: function (show) {
+            this.unblock();
+            
+            result = Meteor.http.call("GET", "http://api.themoviedb.org/3/tv/" + show + "?api_key=" + process.env.MOVIEDB_KEY);
             //console.dir(result.content);
 
             return JSON.parse(result.content);
